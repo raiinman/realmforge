148ecd2d18e53ab370932da55e144990da8b1c45
ShiroMaō Studios <42227255+raiinman@users.noreply.github.com>
2026-09-09 23:14:57 -0700

    rustfmt standalone Gate process

--- components/gate-independent/src/bin/realmforge-gate.rs
+++ components/gate-independent/src/bin/realmforge-gate.rs
@@ -5,8 +5,7 @@ use realmforge_gate_independent::{Issuer, ProviderMetadata, gate_http_router};
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let bind = env::var("REALMFORGE_GATE_BIND").unwrap_or_else(|_| "127.0.0.1:8080".to_owned());
-    let issuer = env::var("REALMFORGE_GATE_ISSUER")
-        .unwrap_or_else(|_| format!("http://{bind}"));
+    let issuer = env::var("REALMFORGE_GATE_ISSUER").unwrap_or_else(|_| format!("http://{bind}"));
 
     let issuer = Issuer::new(issuer)?;
     let metadata = ProviderMetadata::authorization_code(&issuer);
